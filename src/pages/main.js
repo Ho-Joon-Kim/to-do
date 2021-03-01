c9577a27cc0a5d76862040db66bc6b8ad2d83e1e
TitanV <a01098930999@gmail.com>
2021-03-01 20:19:19 +0900

    add - main page

---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Clock from '../component/Clock.js';
+import Todo from '../component/Todo.js';
+
+const Main = () => {
+  return (
+    <div>
+      <Clock />
+      <Todo />
+    </div>
+  );
+};
+
+export default Main;
