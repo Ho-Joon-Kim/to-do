d709d727970dbeb84a125227bea5cc7cac9e7543
TitanV <a01098930999@gmail.com>
2021-03-04 20:19:19 +0900

    add - login

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginC from '../component/Login.js';
+
+const Login = () => {
+  return (
+    <div>
+      <LoginC/>
+    </div>
+  );
+};
+
+export default Login;
